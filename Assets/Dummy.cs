20197e28067ecfc840648e16761e1b40d3402e92
Antoine SARRAZIN <tony78.itouch@gmail.com>
2016-11-22 12:21:46 +0100

    FlappySphere
    Premiers Scripts C#
    - Mecanique Flappy Bird
    - Physiques
    - Particules

---
+++ Assets/Dummy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dummy : MonoBehaviour {
+
+    Transform t;
+    public Vector3 deplacement = new Vector3(1, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Transform>();
+        if (t == null) Debug.Log("No Transform");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.position = t.position + deplacement * Time.deltaTime;
+        
+	}
+}
---
+++ Assets/DummyPhysics.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DummyPhysics : MonoBehaviour
+{
+
+    Rigidbody r;
+    public float speed = 0.5f;
+    public float jumpImpulse = 0.5f;
+    public Transform prefabDeath;
+
+    // Use this for initialization
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //r.AddForce((Camera.main.transform.right)*2);
+        Vector3 rightCam = Camera.main.transform.right * speed * 4.0f;
+        r.velocity = new Vector3(rightCam.x, r.velocity.y, rightCam.z);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            r.AddForce(new Vector3(0, 20.0f * jumpImpulse, 0), ForceMode.Impulse);
+        }
+        Vector3 gaze = r.velocity.normalized;
+        gaze.y /= 2.0f;
+        gaze.y = Mathf.Max(0, gaze.y);
+        transform.LookAt(transform.position + gaze);
+    }
+
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Destroy(this.gameObject);
+        Instantiate(prefabDeath, transform.position, Quaternion.identity);
+    }
+
+}
